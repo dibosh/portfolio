fd279ed77220001c681b35897027776a90a122fa
dibosh <dibosh@newscred.com>
2016-09-09 00:35:47 +0600

    Make npm start to take port as param

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 var express = require('express');
 var app = express();
-var port = 8080;
+var port = process.argv[2] || 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+  console.log('Portfolio listening on port ' + port);
 });
