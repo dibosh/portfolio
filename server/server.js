579ba4c01a326212a2e3645ea55724f0f4fc03c8
dibosh <dibosh@newscred.com>
2016-09-10 00:42:04 +0600

    Dedicated server.js    - Add global error handling, uncaught exceptions will be caught in this      middleware    - Add CORS support    - Separate out other portions as different controller

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var projectCounts = require('./controllers/project.count');
+
+// CORS Support
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/api/count', projectCounts);
+
+// Error handling
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send({
+    'message': 'Internal Server Error'
+  });
+});
+
+app.listen(port);
+console.log('Server started on port ' + port);
