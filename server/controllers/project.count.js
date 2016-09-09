f090fc47e7477f5f2a6816a6393c855de555ef3a
dibosh <dibosh@newscred.com>
2016-09-10 01:37:31 +0600

    Expose API to get behance, dribbble and aggregated count for all sites

--- server/controllers/project.count.js
+++ server/controllers/project.count.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request-promise');
+var q = require('q');
 var commonData = require('../datasource/common.json');
 
 router.use(function timeLog(req, res, next) {
@@ -9,7 +10,7 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/github', function (req, res) {
-  var requestConfig = prepareRequestConfig(null, commonData.portfolios.github.url, {'User-Agent': 'dibosh'});
+  var requestConfig = prepareRequestConfig(null, commonData.portfolios.github.url, null);
   request(requestConfig)
     .then(function (response) {
       res.status(response.statusCode).send({
@@ -27,7 +28,7 @@ router.get('/bitbucket', function (req, res) {
   request(requestConfig)
     .then(function (response) {
       res.status(response.statusCode).send({
-        'count': response.body.length,
+        'count': response.body.size,
         'url': commonData.portfolios.bitbucket.publicProfile
       });
     })
@@ -36,17 +37,90 @@ router.get('/bitbucket', function (req, res) {
     });
 });
 
+router.get('/behance', function (req, res) {
+  var requestConfig = prepareRequestConfig(null, commonData.portfolios.behance.url, null);
+  request(requestConfig)
+    .then(function (response) {
+      res.status(response.statusCode).send({
+        'count': response.body.projects.length,
+        'url': commonData.portfolios.behance.publicProfile
+      });
+    })
+    .catch(function (errorResponse) {
+      res.status(errorResponse.statusCode).send(errorResponse.error.error);
+    });
+});
+
+router.get('/dribbble', function (req, res) {
+  var requestConfig = prepareRequestConfig(null, commonData.portfolios.dribbble.url, null);
+  request(requestConfig)
+    .then(function (response) {
+      res.status(response.statusCode).send({
+        'count': response.body.length,
+        'url': commonData.portfolios.dribbble.publicProfile
+      });
+    })
+    .catch(function (errorResponse) {
+      res.status(errorResponse.statusCode).send(errorResponse.error.error);
+    });
+});
+
+router.get('/all', function (req, res) {
+  var sites = ['behance', 'dribbble', 'github', 'bitbucket'];
+  var promises = [];
+
+  for (var i = 0; i < sites.length; i++) {
+    var requestConfig = prepareRequestConfig(null, commonData.portfolios[sites[i]].url, null);
+    var promise = request(requestConfig)
+      .then(function (response) {
+        return response;
+      });
+
+    promises.push(promise);
+  }
+
+  q.all(promises)
+    .then(function (aggregatedResponse) {
+      var actualResponse = {};
+      for (var index in aggregatedResponse) {
+        var response = aggregatedResponse[index];
+        var hostNameRegex = /\.([^\.]+)\./g;
+        var hostName = hostNameRegex.exec(response.request.uri.hostname)[1];
+        actualResponse[hostName] = dataRetrievalFunctions[hostName](response.body);
+      }
+
+      res.status(200).send(actualResponse);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
+});
+
+var dataRetrievalFunctions = {
+  behance: function (data) {
+    return data.projects.length;
+  },
+  dribbble: function (data) {
+    return data.length;
+  },
+  github: function (data) {
+    return data.length;
+  },
+  bitbucket: function (data) {
+    return data.size;
+  }
+};
+
 function prepareRequestConfig(method, url, headers) {
-  var requestConfig = {
+  return {
     method: method || 'GET',
     url: url,
     json: true,
-    resolveWithFullResponse: true
+    resolveWithFullResponse: true,
+    headers: headers || {
+      'User-Agent': 'dibosh'
+    }
   };
-  if (headers) {
-    requestConfig['headers'] = headers;
-  }
-  return requestConfig;
 }
 
 module.exports = router;
