8c9d1bdf5298c05c8496c9d0ab673189d310ec6b
dibosh <dibosh@newscred.com>
2016-09-11 14:57:59 +0600

    Define angular service to fetch all project counts

---
+++ src/js/services/main.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+  angular
+    .module('portfolioApp.services')
+    .factory('projectCountService', projectCountService);
+
+  projectCountService.$inject = ['$http', 'config'];
+
+  function projectCountService($http, config) {
+    function getProjectCounts() {
+      var url = config.BASE_URL + '/count/all'
+      return $http.get(url)
+        .then(function (response) {
+          return response.data;
+        })
+    }
+
+    return {
+      getProjectCounts: getProjectCounts
+    }
+  }
+})();
