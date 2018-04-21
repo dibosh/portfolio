91002c9a46e6803eda60dcce39176b37b12b9261
dibosh <dibosh@newscred.com>
2018-04-21 19:19:16 +0600

    Add jekyll blog as proxy and fix a few things

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var httpProxy = require('http-proxy');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -17,6 +18,11 @@ var router = require('./controllers/index');
 // The client app
 app.use(express.static('./dist'));
 
+var proxy = httpProxy.createProxyServer({});
+app.use('/blog', function (req, res) {
+  proxy.web(req, res, { target: 'http://127.0.0.1:4000/blog' });
+});
+
 // CORS Support
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -27,10 +33,14 @@ app.use(function (req, res, next) {
 app.use('/api', router);
 
 // Error handling
-app.use(function (err, req, res) {
-  console.error(err.stack);
+app.use(function (err, req, res, next) {
+  if (!err) {
+    next();
+  }
+
+  console.log('Error: %s %O', err.message, err.stack);
   res.status(500).send({
-    'message': err.message
+    'message': 'Internal Server Error'
   });
 });
 
